12e074354ce1ecca65512d2286ea18cd8d20b835
JetechNY <sunjetliu@gmail.com>
2020-09-24 17:30:26 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+
+def actors_list 
+    self.actors.map {|name| name.full_name}
+end
+# A show has many characters and has many actors through characters.
 end
